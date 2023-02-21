b43bc6d5b0c6033fb316897daadc2b67e6d04a80
Jay1 <niwatix@gmail.com>
2023-02-20 23:13:15 -0500

    New aliases

--- aliases/main.fish
+++ aliases/main.fish
@@ -1,5 +1,19 @@
+# Directory movements
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# Goto
+alias dl='cd ~/Downloads'
+alias files ='cd ~/Desktop/Files/'
+alias fdot='cd ~/.config/fish/'
+alias vdot='cd ~/.config/nvim/'
+alias kdot='cd ~/.config/kitty/'
+
+# List
 alias ls="ls -fg"
+
+# Weather
+alias weather="curl -4 wttr.in/Montreal"
+alias moon="curl -4 wttr.in/Moon"
+alias et='sudo rm -rfv /Volumes/*/.Trashes; rm -rfv ~/.Trash'
