7f52705fed74d00840003be78024b85b9546d386
Jay1 <niwatix@gmail.com>
2024-07-02 11:15:42 -0400

    Added a few MacOS specifics

--- aliases/main.fish
+++ aliases/main.fish
@@ -1,3 +1,6 @@
+# Terminal Commands
+alias pwdc="command pwd | tee /dev/tty | pbcopy"
+
 # Directory movements
 alias ...="cd ../.."
 alias ....="cd ../../.."
@@ -9,6 +12,7 @@ alias files='cd ~/Desktop/Files/'
 alias fdot='cd ~/.config/fish/'
 alias vdot='cd ~/.config/nvim/'
 alias kdot='cd ~/.config/kitty/'
+alias dotvault='cd ~/Dotvault/'
 
 # List
 alias ls="ls -fg"
@@ -17,3 +21,8 @@ alias ls="ls -fg"
 alias weather="curl -4 wttr.in/Montreal"
 alias moon="curl -4 wttr.in/Moon"
 alias et='sudo rm -rfv /Volumes/*/.Trashes; rm -rfv ~/.Trash'
+
+#MacOS specific
+alias update="sudo softwareupdate -i -a; brew update; brew upgrade"
+alias flush="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+alias trash="rm -rf ~/.Trash/*"
